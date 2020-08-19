ba1c05abcdeaf6279b9f377c1c5f1075d9955ea4
zibrahim2046 <zibrahim2046@gmail.com>
2020-08-19 12:35:14 -0500

    added form and button inputs

--- src/App.js
+++ src/App.js
@@ -5,12 +5,16 @@ import './App.css';
 const App = () => {
     const APP_ID = 'ea64e37b';
     const APP_KEY = '2999b28eb1a5b454fa702c85da45ebad';
-    const exampleRequest =
-        'https://api.edamam.com/search?q=chicken&app_id=${YOUR_APP_ID}&app_key=${YOUR_APP_KEY}&from=0&to=3&calories=591-722&health=alcohol-free';
+    const exampleRequest = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`;
 
     return (
         <div className='App'>
-            <h1> Hello React</h1>
+            <form className='search-form'>
+                <input className='search-bar' type='text' />
+                <button className='search-button' type='submit'>
+                    Search
+                </button>
+            </form>
         </div>
     );
 };
